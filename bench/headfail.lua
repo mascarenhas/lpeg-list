7746854aa5b7c6e33eab9b4afa501905ec82f19a
Fabio Mascarenhas <mascarenhas@lambda.local>
2009-03-31 20:50:59 -0300

    added third string to headfail test

--- bench/headfail.lua
+++ bench/headfail.lua
@@ -3,11 +3,12 @@ local m = require "listlpeg"
 
 local s1 = "abcdefghijklmnopqrstuvwxyz"
 local s2 = "zyxwvutsrqdonmlkjihgfedcba"
+local s3 = s1 .. s2
 
-local p = m.L(s1) + m.L(s2)
+local p = m.L(s1) + m.L(s2) + m.L(s3)
 
 local assert = assert
 
 return function ()
-	 assert(p:match{ s1 } and p:match{ s2 })
+	 assert(p:match{ s1 } and p:match{ s2 } and p:match{ s3 })
        end
