de77b9ee0731e5e6949866e63a5cdc618b47002e
mascarenhas User <mascarenhas@ubuntu.ubuntu-domain>
2009-04-01 22:40:11 -0300

    partialclosecommit bench

---
+++ bench/partial.lua
@@ -0,0 +1,26 @@
+
+local m = require "listlpeg"
+
+local function rands(size)
+  local digits = {}
+  for i = 1, size do digits[i] = string.byte("a") + math.random(26) - 1 end
+  return string.char(unpack(digits))
+end
+
+local s = {}
+
+for i = 1, 5 do s[i] = rands(5) end 
+
+local p = m.L(m.L(s[1]) * m.L(s[2]) * m.L(s[3]) * m.L(s[4]) * m.L(s[5]))^0
+
+local assert = assert
+
+m.print(p)
+
+subject = {}
+
+for i = 1, 100 do subject[i] = s end
+
+return function ()
+	  assert(p:match(subject)) 
+       end
